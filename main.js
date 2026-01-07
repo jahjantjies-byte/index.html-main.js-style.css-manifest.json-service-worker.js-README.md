aad4fda3f78c008356d4ff8efc9e424f55bebb64
jahjantjies-byte <jahjantjies@gmail.com>
2026-01-06 16:45:40 -0800

    Update main.js
    Register service worker

--- main.js
+++ main.js
@@ -63,3 +63,10 @@ if ('serviceWorker' in navigator) {
       .catch(err => console.log('Service Worker failed:', err));
   });
 }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(reg => console.log('Service Worker registered', reg))
+      .catch(err => console.log('Service Worker failed:', err));
+  });
+}
